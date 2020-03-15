627ccc2688b78ed1a10946f76481edcd9507cd31
shlomo neuberger <shlomo.neuberger@gmail.com>
2020-03-15 23:05:27 +0200

    First Commit

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+
+namespace App1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        
+        
+        private void OnSaveClick(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void OnDeleteClick(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
